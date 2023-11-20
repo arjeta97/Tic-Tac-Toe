268fac73f0a0540d7fdaccbe52fc921a5fe83ebe
DESKTOP-22NH2NS\Arjeta <arjeta.allamani@1up.al>
2023-11-20 16:01:38 +0100

    Final product

--- main.js
+++ main.js
@@ -1,123 +1,235 @@
+let player_1 = document.getElementById('player-name');
+let player_2 = document.getElementById('player-name2');
 
-// Player 1 Submit the name
+let form_1 = document.getElementById('form');
+let form_2 = document.getElementById('form2');
 
-// Wait for the DOM to load
-  document.addEventListener('DOMContentLoaded', function() {
-    // Get the form and result div elements
-    var form = document.getElementById('form');
-    var resultDiv = document.getElementById('ply-name');
-    // Add an event listener for form submission
-    form.addEventListener('submit', function(event) {
-      // Prevent the default form submission
-      event.preventDefault();
-      const formData = new FormData(form);
-      const formDataObject = {};
+let player_1_module = document.getElementById('player-modal');
+let player_2_module = document.getElementById('player2-modal');
 
-      for (const[key, value] of formData.entries()){
-        formDataObject[key] = value
-      }
-      console.log(formDataObject);
-      // Retrieve the form values
-      var name = document.getElementById('player-name').value;
-        // console.log(name)
-      // Display the form data in the result div
-      resultDiv.innerHTML = name ;
+let display_player_1 = document.getElementById('ply-name');
+let display_player_2 = document.getElementById('ply-name2');
+
+let strGameBtn = document.getElementById('start-new-btn');
+let gameField = document.getElementById('game-field');
+
+let gameFieldElements = document.querySelectorAll('.square');
+let switch_player_name = document.getElementById('player-name-switch');
+
+let winnerBanner = document.getElementById("winner-banner");
+let winnerName = document.getElementById("winner-name");
+
+let StartNewGameBtn = document.getElementById("start-new-btn");
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var activePlayer = 0;
+  var currentRound = 1;
+
+  var players = [
+    {
+      name: '',
+      symbol: 'X'
+    },
+    {
+      name: '',
+      symbol: 'O'
+    }
+  ];
+
+  const gameData = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0],
+  ];
+
+
+  form_1.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (player_1.value == "") {
+      alert("Please enter your name");
+      return;
+    } if(player_2.value == player_1.value ){
+
+      alert("Please enter different name from the other player");
+      return;
+    }else {
+      player_1_module.style.display = "none";
+      const modalBackdrops = document.querySelectorAll('.modal-backdrop');
+      modalBackdrops.forEach(function (backdrop) {
+        backdrop.remove();
+      });
+    }
+    display_player_1.innerHTML = player_1.value;
+
+    const data = new FormData(e.target);
+    const name = data.get("name");
+    players[0].name = name; 
   });
+
+  form_2.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (player_2.value == "") {
+      alert("Please enter your name");
+      return;
+    } if(player_1.value == player_2.value ){
+
+      alert("Please enter different name from the other player");
+      return;
+    }else {
+      player_2_module.style.display = "none";
+      const modalBackdrops2 = document.querySelectorAll('.modal-backdrop');
+      modalBackdrops2.forEach(function (backdrop) {
+        backdrop.remove();
+      });
+    }
+
+    display_player_2.innerHTML = player_2.value;
+
+    const data = new FormData(e.target);
+    const name = data.get("name");
+    players[1].name = name; 
+  });
+
+  function switchPlayer() {
+    if (activePlayer === 0) {
+      activePlayer = 1;
+    } else {
+      activePlayer = 0;
+    }
+    switch_player_name.innerHTML = players[activePlayer].name;
+  }
+  
+  function ResetGame (){
+    activePlayer = 0;
+    currentRound = 1;
+    winnerBanner.style.display = "none";
+    document.getElementById("game-field").style.pointerEvents = "all";
+    document.querySelectorAll('.square').forEach(function(square) {
+      square.classList.remove("disabled");
+    });
+
+    let gameBoxIndex = 0;  
+    for (let i = 0; i < 3; i++){
+      for (let j = 0; j < 3; j++){
+        gameData[i][j] = 0;
+      }
+      for( gameFieldElement of gameFieldElements){
+        if(gameBoxIndex <gameFieldElements.length) {
+          gameFieldElement.textContent = "";
+          gameBoxIndex++;
+        }
+      }
+    }
+
+  };
+
+  
+  function StartNewGame() {
+    if (players[0].name === "" || players[1].name === "") {
+      alert('Please fill out the form');
+      return;
+    }
+    ResetGame();
+    switch_player_name.innerHTML = players[activePlayer].name;
+    document.getElementById("turn-container").style.display = "block"
+    gameField.style.display = "grid";
+
+  }
+
+ 
+  strGameBtn.addEventListener('click', StartNewGame);
+
+  for (const gameFieldElement of gameFieldElements) {
+    gameFieldElement.addEventListener('click', selectGameField);
+  }
+
+  function selectGameField(event) {
+    event.target.textContent = players[activePlayer].symbol;
+    event.target.classList.add('disabled');
+    const selectedField = event.target;
+    const selectedColumn = selectedField.dataset.col -1;
+    const selectedRow = selectedField.dataset.row -1;
+  
+    gameData[selectedColumn][selectedRow] = activePlayer + 1;
+
+    const testcheckWinning = checkWinng();
+
+    currentRound++;
+  
+    switchPlayer();
+    if(testcheckWinning !== 0){
+      EndGame(testcheckWinning)
+    }
+  }
+  
+  function checkWinng (){
+    for(let i=0; i<3; i++){
+      if(
+        gameData[i][0] > 0 &&
+        gameData[i][0] === gameData[i][1] &&
+        gameData[i][0] === gameData[i][2]
+      ){
+        return gameData[i][0]
+      }
+    }
+  
+    for (let i=0; i<3; i++){
+      if(
+        gameData[0][i] > 0 && 
+        gameData[0][i] === gameData[1][i]  &&
+        gameData[1][i] === gameData[2][i]
+      )
+      return gameData [0][i]
+    }
+  
+    if(
+      gameData[0][0] > 0 &&
+      gameData[0][0] === gameData[1][1] &&
+      gameData[1][1] === gameData[2][2]
+    ){
+      return gameData[0][0];
+    }
+  
+    if(
+      gameData[2][0] > 0 &&
+      gameData[2][0] === gameData [1][1] &&
+      gameData[1][1] === gameData [0][2]
+    ){
+      return gameData[2][0];
+    }
+
+    if(currentRound === 9){
+      return -1;
+    }
+    return 0;
+  }
+
+  function EndGame(checkWinng) {
+    if(checkWinng === -1){
+      winnerBanner.style.display = "flex";
+      document.getElementById("turn-container").style.display = "none";
+      winnerName.textContent = "It is a draw so please try again";
+      gameField.style.pointerEvents = 'none'
+    } else {
+      winnerBanner.style.display = "flex";
+      document.getElementById("turn-container").style.display = "none";
+      const winner = players[checkWinng -1].name;
+      gameField.style.pointerEvents = 'none'
+      winnerName.textContent = "🎉" + winner + ', you won' + "🎉";
+      function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+      }
+      
+      confetti({
+        angle: randomInRange(55, 125),
+        spread: randomInRange(50, 50),
+        particleCount: randomInRange(200, 200),
+        origin: { y: 0.5 },
+      });
+    }
+  };
+
 });
 
-// Close the modal on submit for player one
-var SubmitBtn = document.getElementById('submit');
-function closeModal(){
-  var enteredName = document.getElementById('player-name').value;
-  if(enteredName =! '' && enteredName.length>3){
-    SubmitBtn.setAttribute("data-bs-dismiss", "modal");
-  }
-  else{
-    alert('Plese fill out the form')
-  }
-}  
-SubmitBtn.addEventListener('click', closeModal)
-
-
-
-// Player 2 Submit the name
-
-// Wait for the DOM to load
-document.addEventListener('DOMContentLoaded', function() {
-// Get the form and result div elements
-var form2 = document.getElementById('form2');
-var resultDiv2 = document.getElementById('ply-name2');
-// Add an event listener for form submission
-
-form2.addEventListener('submit', function(event) {
-  const formData = new FormData(form2);
-  const formDataObject2 = {};
-
-  for (const[key, value] of formData.entries()){
-    formDataObject2[key] = value
-  }
-// Prevent the default form submission
-
-event.preventDefault();
-// Retrieve the form values
-var name2 = document.getElementById('player-name2').value;
-// console.log(name)
-// Display the form data in the result div
-resultDiv2.innerHTML = name2 ;
-});
-});
-
-// Close the modal on submit for player two
-var SubmitBtn2 = document.getElementById('submit2');
-function closeModal2(){
-let enteredName2 = document.getElementById('player-name2').value;
-console.log(enteredName2);
-
-if(enteredName2 = !'' && enteredName2.length>3){
-  SubmitBtn2.setAttribute("data-bs-dismiss", "modal");
-}
-else{
-  alert('Plese fill out the form')
-}
-}  
-SubmitBtn2.addEventListener('click', closeModal2)
-
-
-// Start a new game
-
-const strNewBtn = document.getElementById('start-new-btn');
-const gameField = document.getElementById('game-field');
-
-function startNewGameBtn (){
-  let secondPlayer = document.getElementById('player-name2').value;
-  let firstPlayer = document.getElementById('player-name').value;
-  if( firstPlayer == "" && secondPlayer == ""){
-  alert('Please fill out the name fields')
-}
-else
-{
-  gameField.classList.add('game-field1')
-}
-}
-
-strNewBtn.addEventListener('click', startNewGameBtn)
-
-//Game rounds
-let activePlayer = 0;
-
-const players = [
-  {
-    name: '',
-    symbol: 'X'
-  },
-  {
-    name: '',
-    symbol: 'Y'
-  },
-];
-
-const gameFieldElements = document.getElementById('square');
-
-for(const gameFieldElement of gameFieldElements){
-  gameFieldElement.addEventListener('click', )
-}
